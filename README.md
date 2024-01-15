f17301ea667a5d7b6235ef28035fc79d97f37652
l3miage-barryibr <95280002+l3miage-barryibr@users.noreply.github.com>
2024-01-15 15:49:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ihmexoVecteur2D
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/l3miage-barryibr/ihmexoVecteur2D)
---
+++ index.ts
@@ -0,0 +1,16 @@
+// IMPORTEZ Vecteur2D et Vecteur2D_C
+//
+import { Vecteur2D_C, Vecteur2D } from './src/Vecteur2D';
+
+/*
+ * Testez la création de V1 (coordonnée <3, 2>)
+ * Testez la création de V2 (coordonnée <9, 8>)
+ * Vérifiez que V1.ajouter(V2)
+ *    renvoie bien un Vecteur2D de coordonnée <12, 10>
+ */
+const V1: Vecteur2D = new Vecteur2D_C(3, 2);
+const V2: Vecteur2D = new Vecteur2D_C(9, 8);
+console.log(V1);
+console.log(V2);
+const a = V1.ajouter(V2);
+console.log(a);
